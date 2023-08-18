13eacbb3d55fc8dd651e5d28c571f48356da4704
Kono <konotori08@gmail.com>
2023-08-18 19:43:59 +0900

    third commit

--- fibo.py
+++ fibo.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 def fib(n):
     a, b = 0, 1
     while a < n:
         print(a, end=" ")
         a, b = b, a+b
     print()
-
 
 def fib2(n):
     result = []
---
+++ test.py
@@ -0,0 +1,5 @@
+print("Hello Wolrd")
+
+print(22 // 3)
+
+print(4**2)
