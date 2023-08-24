5de96271b72407c1e5a76d72070e29759068cec9
Kono <konotori08@gmail.com>
2023-08-24 16:02:41 +0900

    Second Commit

---
+++ 8章/エラーと例外/as.py
@@ -0,0 +1,27 @@
+# -*- coding: Shift-JIS -*-
+
+
+#x = 10 / 0
+#ZeroDivisionError: division by zZeroDivisionErroro, eee
+
+# except[例外の種類]as[変数名]:
+try:
+    x = 10 / 0
+except ZeroDivisionError as eee:
+    print(type(eee))
+    print(eee.args) #例外の説明を要素に持つタプル
+    print(eee.args[0])
+    print(eee)
+
+
+#[ZeroDivisionError: division by zero]を出力
+try:
+    x = 10 / 0
+except ZeroDivisionError as eee:
+    print("ZeroDivisionError", eee)
+
+try:
+    x = 10 / 0
+    #全ての例外に対してexcept節のコードを実行させる
+except Exception as eee:
+    print("ZeroDivisionError", eee)
--- 8章/エラーと例外/elseFinally.py
+++ 8章/エラーと例外/elseFinally.py
@@ -1,5 +1,7 @@
 # -*- coding: Shift-JIS -*-
 
+print("-------コード05--------")
+#try節で例外が生じなかった場合
 try:
     x = 2
     print("10 / x =", 10 / x)
@@ -13,4 +15,41 @@ finally:
     #必ず実行
     print("finally節の実行")
 
+print("プログラムの終了")
+
+print("-------コード06--------")
+#try節で例外発生→except節で指定すると例外と一致
+try:
+    #TypeErrorの発生
+    x = "2"
+    print("10 / x =", 10 / x)
+    print("Try節終了")
+except TypeError:
+    #実行
+    print("TypeErrorです")
+else:
+    #try節で例外発生したのでelse節は実行せず
+    print("else節の実行")
+finally:
+    #必ず実行される
+    print("finally節の実行")
+
+print("プログラムの終了")
+
+print("-------コード07--------")
+try:
+    x = 0
+    print("10 / x =", 10 / x)
+    print("Try節終了")
+#例外の種類の不一致→実行せず
+except TypeError:
+    print("TypeErrorです")
+else:
+    #これも実行せず
+    print("else節の実行")
+finally:
+    #必ず実行される
+    print("finally節の終了")
+
+#　例外を送出。実行を停止
 print("プログラムの終了")
