6e2b3c6dddda51c82f9aa84435fe0cb4613278f9
Kono <konotori08@gmail.com>
2023-08-30 15:12:31 +0900

    Commit

--- 10章/module.py
+++ 10章/module.py
@@ -1,2 +1,8 @@
 import sys
 print(sys.argv)
+
+
+import argparse
+parser = argparse.ArgumentParser(description='Process some integers.')
+args = parser.parse_args()
+print(args.accumulate(args.integers))
