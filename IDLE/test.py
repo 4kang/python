e8ecbd830a86b8e5a9784514c516a178a2fc8c1b
Kono <konotori08@gmail.com>
2023-09-15 00:59:52 +0900

    replace Commit

--- IDLE/test.py
+++ IDLE/test.py
@@ -1,7 +1,19 @@
 import this
 
+#文字列
 print("HelloWorld")
 print("こんにちわ～～眠いですがぼちぼちやってますコノヤロー")
 print("Hello", "world")
 print("Hello" + "World")
 print("I" + "am", "Python")
+
+#分割メソッド
+print("Hello Wolrd".split(" "))
+print("Hello World".split("e")) #eが消える
+print("Hello Wolrd".split())
+print("Java Script".split())
+print("ABCDEFGHI".split("F"))
+
+#置換メソッド
+print("Hello World".replace("o", "A"))
+print("Hello World".replace("l", "L"))
