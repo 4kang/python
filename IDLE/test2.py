8a3277a88db0832b40998ff37ad07401a271eae4
Kono <konotori08@gmail.com>
2023-10-14 21:59:01 +0900

    commit

--- IDLE/test2.py
+++ IDLE/test2.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+#リスト
+print(["Hello"])
+
+print(type(["Hello"]))
+
+#リストで複数の文字列を入れる
+print(["Hello", "Wolrd", "これを要素という"])
+
+print(["a", "b", "c", "d", "e"])
+
+#数値を入れる
+print(["a", "b", "c", 1, 2, 3])
